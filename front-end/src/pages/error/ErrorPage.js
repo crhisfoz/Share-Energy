6882d4b00e7d3ba2e2c4e9c3bef6e1e32fa80724
_crhis_ <crhisnokia@live.com>
2023-01-19 07:39:29 -0300

    feat: criando nova pagina de not_found

--- front-end/src/pages/error/ErrorPage.js
+++ front-end/src/pages/error/ErrorPage.js
@@ -1,10 +1,30 @@
 import React from "react";
 import { StyledError } from "./styledError";
+import "./style.css"
 
 export const ErrorPage = () => {
 
     return (
-        <StyledError>
-        </StyledError>
+            <section className="page_404">
+                <div className="container">
+                    <div className="row"></div>
+                    <div className="col-sm-12">
+                        <div className="col-sm-10 col-sm-offset-1 text-center">
+                            <div className="four_zero_four_bg">
+                                <h1 className="text-center"> 404 </h1>
+                                <div className="contant_box_404">
+                                    <h3 className="h2"> Parece que você está perdido</h3>
+                                    <p>a página que você está procurando não está disponível no momento, atualize-a ou aguarde alguns instantes e tente novamente</p>
+                                    <a href="/home" className="link-404">Go to Home</a>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+            </section>
+
     );
 };
