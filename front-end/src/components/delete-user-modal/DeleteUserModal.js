4f17f8d2c01d6cb69abde454421ecb5675ed2e2a
_crhis_ <crhisnokia@live.com>
2023-01-18 22:23:36 -0300

    perf: refatoracao de código colocando opção cancelar ou confirmar no modal de deletar usuário

--- front-end/src/components/delete-user-modal/DeleteUserModal.js
+++ front-end/src/components/delete-user-modal/DeleteUserModal.js
@@ -3,79 +3,112 @@ import Swal from 'sweetalert2';
 import axios from 'axios';
 import { BASE_URL } from '../../constants/urls';
 import { GlobalContext } from '../../context/GlobalContext';
+import Form from 'react-bootstrap/Form';
+import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
 
-export const DeleteUserModal = ({ user, isDelete, setIsDelete }) => {
 
-  const { token, sessionToken } = useContext(GlobalContext);
+export const DeleteUserModal = ({ user, setDelete }) => {
+  
+  const {token, sessionToken } = useContext(GlobalContext);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
 
-  let tokenFunction;
-  (token.length > 0 ? tokenFunction = token[0] : tokenFunction = sessionToken[0])
 
-      const onSubmitDeleteUser = async (ev) => {
-        ev.preventDefault();
-        const headers = {
-          headers:{
-            Authorization: tokenFunction
-          }
-        }
+  let tokenFunction ;
+  (token.length > 0 ? tokenFunction = token[0] : tokenFunction = sessionToken[0] )
 
-        try {
+  const handleShow = () =>{
+      setShow(true);
 
-            const response = await axios.delete(`${BASE_URL}/${user.id}`, headers);
-            if(response){
-              Swal.fire({
-                position: "top-end",
-                icon: "success",
-                title: "Usuário Deletado Com Sucesso",
-                showConfirmButton: false,
-                timer: 1500
-              })
-            setIsDelete(true)
-            }
-      
-          } catch (error) {
-            Swal.fire({
-              position: "top-end",
-              icon: "error",
-              title: "Oops...",
-              text: "Sua Solicitação não foi completada, Confira o Id do usuário e tente novamente",
-              showConfirmButton: false,
-              timer: 1500
-          })
-      
-          }
-        }
+  }
+
+  const onDeleteUser = async (ev)=>{
+
+    console.log(user)
+    console.log(tokenFunction)
+
+    ev.preventDefault();
+
+    const headers = {
+      headers:{
+        Authorization: tokenFunction
+      }
+    }
+
+    try {
+
+      const response = await axios.delete(`${BASE_URL}/${user.id}`, headers);
+      if(response){
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Usuário Deletado Com Sucesso",
+          showConfirmButton: false,
+          timer: 1500
+        })
+        setShow(false)
+        setDelete(true)
+      }
+
+    } catch (error) {
+      console.log(error)
+      Swal.fire({
+        position: "top-end",
+        icon: "error",
+        title: "Oops...",
+        text: "Requisição não completada, verifique o id enviado, Confira e tenta novamente",
+        showConfirmButton: false,
+        timer: 1500
+    })
+
+    }
+  }
 
   return (
     <>
-      {/*     <!-- Button trigger modal --> */}
-      <button type="button" className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop"
-        style={{ backgroundColor: "red" }}>
-        Deletar
-      </button>
+    <td type="submit" className="btn btn-danger" id="edit-button" onClick={()=>handleShow()}
+    style={{backgroundColor: "red"}}
+    >
+            Deletar
+        </td>
 
-      {/*    <!-- Modal --> */}
-      <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="staticBackdropLabel"> Deseja Realmente Deletar o Usuário ?</h5>
-              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div className="modal-body">
-             <h2 style={{color: "black"}}>{user.name} </h2>  
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-              <button type="button" className="btn btn-danger" style={{ backgroundColor: "red" }}
-              onClick={onSubmitDeleteUser}>Confirmar</button>
-            </div>
-          </div>
-        </div>
-      </div>
+        <Modal show={show} onHide={handleClose}   backdrop="static" style={{backgroundColor: "rgba(123, 123, 123, 0.5)"}}>
+    <Modal.Header closeButton>
+      <Modal.Title>Deletar Usuário</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+    <Form.Label>Deseja Realmente Deletar o usuário ?? </Form.Label>
+      <Form id="editusermodal" onSubmit={onDeleteUser}>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            value={user.username}
+            name="username"
+            disabled
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            value={user.email}
+            disabled
+          />
+        </Form.Group>
 
+      </Form>
+    </Modal.Body>
+    <Modal.Footer>
+      <Button variant="secondary" onClick={handleClose}>
+        CANCELAR
+      </Button>
+      <button type="submit" className="btn btn-danger" form="editusermodal">CONFIRMAR</button>
+    </Modal.Footer>
+  </Modal>        
     </>
   );
 }
