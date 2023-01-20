69651156cb53c1f436ff5bba22cefd11392cfe7b
_crhis_ <crhisnokia@live.com>
2023-01-20 19:21:33 -0300

    refactor: página not found pronta

--- front-end/src/pages/error/ErrorPage.js
+++ front-end/src/pages/error/ErrorPage.js
@@ -5,26 +5,24 @@ import "./style.css"
 export const ErrorPage = () => {
 
     return (
-            <section className="page_404">
-                <div className="container">
-                    <div className="row"></div>
-                    <div className="col-sm-12">
-                        <div className="col-sm-10 col-sm-offset-1 text-center">
+        <section className="page_404">
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-12 ">
+                        <div className="col-sm-10 col-sm-offset-1  text-center">
                             <div className="four_zero_four_bg">
-                                <h1 className="text-center"> 404 </h1>
-                                <div className="contant_box_404">
-                                    <h3 className="h2"> Parece que você está perdido</h3>
-                                    <p>a página que você está procurando não está disponível no momento, atualize-a ou aguarde alguns instantes e tente novamente</p>
-                                    <a href="/home" className="link-404">Go to Home</a>
-                                </div>
+                                <h1 className="text-center ">404</h1>
                             </div>
-
+                            <div className="contant_box_404">
+                                <h3 className="h3">
+                                    Parece que Você está perdido                              </h3>
+                                <p>A página que você está procurando não está acessível no momento, aguarde alguns instantes e tente novamente!</p>
+                                <a href="/home" className="link_404">Go To Home</a>
+                            </div>
                         </div>
-
                     </div>
-
                 </div>
-            </section>
-
+            </div>
+        </section>
     );
 };
