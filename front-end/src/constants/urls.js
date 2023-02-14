75adb5aa89759692a030c39edce29308aaca71ef
_crhis_ <crhisnokia@live.com>
2023-02-14 10:26:40 -0300

    refactor: atualizacoes read-me e em url

--- front-end/src/constants/urls.js
+++ front-end/src/constants/urls.js
@@ -1,6 +1,6 @@
-// export const BASE_URL = "https://ec2-54-164-12-137.compute-1.amazonaws.com:3003/users"
+export const BASE_URL = "http://ec2-54-164-12-137.compute-1.amazonaws.com:3003/users"
 
-export const BASE_URL = "http://localhost:3003/users"
+//export const BASE_URL = "http://localhost:3003/users"
 
  export const RANDOM_URL = "https://randomuser.me/api/"
 
